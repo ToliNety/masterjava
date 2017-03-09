edc21bd6d19af4814be2ed9c0a0ce19fdf7e2a83
Anatolii Shirokov <tolikswc@yandex.ru>
2017-03-09 14:33:20 +0300

    fix concurrentCompletedServiceMultiply

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -42,37 +42,55 @@ public class MatrixUtil {
         return matrixC;
     }
 
-    public static int[][] concurrentComplitetServiceMultiply (int[][] matrixA, int[][] matrixB, ExecutorService executor)
-            throws InterruptedException{
+    public static int[][] concurrentComplitetServiceMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor)
+            throws InterruptedException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        final int[][] matrixBT = new int[matrixSize][matrixSize];
+        class ColResult {
+            int colNum;
+            int[] colBody;
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                matrixBT[j][i] = matrixB[i][j];
+            public ColResult(int colNum, int[] colBody) {
+                this.colNum = colNum;
+                this.colBody = colBody;
             }
         }
 
+        CompletionService<ColResult> completionService = new ExecutorCompletionService<ColResult>(executor);
 
-
-        CountDownLatch latch = new CountDownLatch(matrixSize);
         for (int i = 0; i < matrixSize; i++) {
-            final int n = i;
-            executor.submit(() -> {
+            final int col = i;
+            final int[] columnBT = new int[matrixSize];
+
+            for (int j = 0; j < matrixSize; j++) {
+                columnBT[j] = matrixB[j][i];
+            }
+
+            completionService.submit(() -> {
+                final int[] columnC = new int[matrixSize];
                 for (int j = 0; j < matrixSize; j++) {
+                    final int[] rowA = matrixA[j];
                     int sum = 0;
                     for (int k = 0; k < matrixSize; k++) {
-                        sum += matrixA[n][k] * matrixBT[j][k];
+                        sum += rowA[k] * columnBT[k];
                     }
-                    matrixC[n][j] = sum;
+                    columnC[j] = sum;
                 }
-                latch.countDown();
+                return new ColResult(col, columnC);
             });
         }
 
-        latch.await(10, TimeUnit.SECONDS);
+        for (int i = 0; i < matrixSize; i++) {
+            try {
+                final ColResult result = completionService.poll(10, TimeUnit.SECONDS).get();
+                for (int j = 0; j < matrixSize; j++) {
+                    matrixC[j][result.colNum] = result.colBody[j];
+                }
+            } catch (ExecutionException ex) {
+                throw new InterruptedException(ex.getMessage());
+            }
+        }
 
         return matrixC;
     }
